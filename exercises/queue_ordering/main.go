e69c98723246d539a2a29a9b365b2702d82539bd
Louis Sayers <lssayers@gmail.com>
2020-04-09 18:27:50 +1000

    Created a new method that performs the sorting in place

--- exercises/queue_ordering/main.go
+++ exercises/queue_ordering/main.go
@@ -32,8 +32,7 @@ func (p People) Swap(i, j int) {
 	p[i], p[j] = p[j], p[i]
 }
 
-func main() {
-	input := People{{7,0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}}
+func peformSort1(input People) {
 	sort.Sort(input)
 
 	result := make([]*Person, len(input))
@@ -48,4 +47,26 @@ func main() {
 	}
 
 	fmt.Printf("People: %v\n", result)
+}
+
+func peformSort2(input People) {
+	sort.Sort(input)
+
+	for i := 0; i < len(input); i++ {
+		current := input[i]
+
+		for j := current.k; j <= i; j++ {
+			temp := input[j]
+			input[j] = current
+			current = temp
+		}
+	}
+
+	fmt.Printf("People: %v\n", input)
+}
+
+func main() {
+	input := People{{7,0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}}
+	peformSort1(input)
+	peformSort2(input)
 }
